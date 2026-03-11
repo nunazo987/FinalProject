99be3545476b733c60401ba4b88b70cd9d867823
Nuno <nunomarquees@gmail.com>
2026-03-11 14:51:46 +0000

    test: environment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { environment } from './environments/environment';
+
+console.log(environment.API_KEY);
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+
