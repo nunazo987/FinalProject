ebef03dd2fb56ecadf640d94f5f6c8f97c1ab022
Nuno <nunomarquees@gmail.com>
2026-03-11 16:08:14 +0000

    chore: update configs

--- eslint.config.js
+++ eslint.config.js
@@ -5,37 +5,12 @@ const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 
 module.exports = defineConfig([
+  tseslint.configs.base,
   {
-    files: ['**/*.ts'],
-    extends: [
-      eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
-    ],
+    files:['**/*.ts'],
     processor: angular.processInlineTemplates,
     rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
+      '@typescript-eslint/no-unused-vars': 'error',
     },
-  },
-  {
-    files: ['**/*.html'],
-    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
-    rules: {},
-  },
+  }
 ]);
