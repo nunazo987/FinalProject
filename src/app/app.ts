915af3263e8275744292e3ea93c7427416e9dfd5
Nuno <nunomarquees@gmail.com>
2026-03-18 07:35:37 +0000

    Fix12 YAML syntax for environment generation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar'
-import { environment } from '../environments/environment.development';
 import { Router } from '@angular/router'
 
 @Component({
@@ -15,29 +14,3 @@ export class App {
 
   router = inject(Router);
 }
-
-async function loadUsers() {
-  const response = await fetch(environment.apiUrl + '/users');
-  const users = await response.json();
-
-  console.log(users);
-}
-
-loadUsers();
-
-async function createUser(){
-  const user = { id: "4", username: "ana", password: "5678" };
-  
-  const response = await fetch(environment.apiUrl + '/users', {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(user)
-});
-
-const data = await response.json();
-console.log(data);
-}
-
-createUser();
